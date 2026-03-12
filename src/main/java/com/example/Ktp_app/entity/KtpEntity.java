0c5d17f1a379fe2a5fafdd7bb1b5d494a39801e3
SandyArdiyan <sanardiyan881@gmail.com>
2026-03-12 22:59:57 +0700

    Mengapus beberapa

--- src/main/java/com/example/Ktp_app/entity/KtpEntity.java
+++ src/main/java/com/example/Ktp_app/entity/KtpEntity.java
@@ -6,14 +6,13 @@ import java.time.LocalDate;
 
 @Entity
 @Table(name = "ktp")
-@Data // Menggunakan Lombok untuk Getter/Setter
+@Data
 public class KtpEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(unique = true, nullable = false)
-    private String nomorKtp;
 
     private String namaLengkap;
     private String alamat;
